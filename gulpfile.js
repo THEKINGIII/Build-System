1f7631623c192c9aaf39b3b4b10c081f2bddfd62
Majd Fouqhaa (IKSA) <majd.fou@gmail.com>
2017-05-03 07:50:50 -0400

    Adding intial template for build system

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by majdfouqhaa on 5/3/17.
+ */
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+// Now we can initialize a gulp task -> lets do that
+gulp.task('log',function () {
+   gutil.log('Welcome to final destination');
+});
